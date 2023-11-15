6faff3e9b978c5d6963ca388d20ba99413ac606c
Pongsaky <pongsakon.kawe@gmail.com>
2023-11-15 22:45:06 +0700

    Tree

--- test.cpp
+++ test.cpp
@@ -1,15 +1,10 @@
 #include <iostream>
 #include <cmath>
+using namespace std;
 
 int main() {
-    int mSize = 7;
-    int k = -10;
-
-    int result1 = (mSize - abs(k) % mSize) % mSize;
-    int result2 = (k + mSize) % mSize;
-
-    std::cout << "Result 1: " << result1 << std::endl;
-    std::cout << "Result 2: " << result2 << std::endl;
+    
+    cout << (true && false);
 
     return 0;
 }
